9b429599f10f72cfaff7f2c422004163da0708b8
Divyanshu Baghel <divyanshubaghel1@gmail.com>
2022-12-05 14:55:32 +0530

    added the frontend of woekout app

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import Workoutdetails from '../components/Workoutdetails'
+
+
+
+function Home() {
+  const [workouts, setWorkouts]=useState(null)
+
+  useEffect(()=>{
+    const fetchWorkouts= async () =>{
+      const response = await fetch ('/api/workouts')
+      const json =await response.json()
+
+      if (response.ok){
+        setWorkouts(json)
+      }
+    }
+    fetchWorkouts()
+  },[])
+  return ( 
+    <div className='home'>
+        <div className='workouts'>
+          {workouts && workouts.map((workout)=>( 
+            <Workoutdetails key={workout._id} workout={workout} />
+          ))}
+        </div>
+    </div>
+  )
+}
+
+export default Home
