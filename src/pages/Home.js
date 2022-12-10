1d9214e233f7d9aad2e5089f5d7d7dc3cd23e6f7
Divyanshu Baghel <divyanshubaghel1@gmail.com>
2022-12-10 13:57:48 +0530

    Complete frontend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import {useWorkoutsContext} from "../hooks/useWorkoutsContext"
+
 import Workoutdetails from '../components/Workoutdetails'
+import Workoutform from '../components/Workoutform'
 
 
 
 function Home() {
-  const [workouts, setWorkouts]=useState(null)
+ // const [workouts, setWorkouts]=useState(null)
+  const {workouts, dispatch} = useWorkoutsContext()
 
   useEffect(()=>{
     const fetchWorkouts= async () =>{
@@ -12,11 +16,11 @@ function Home() {
       const json =await response.json()
 
       if (response.ok){
-        setWorkouts(json)
+        dispatch({type: 'SET_WORKOUTS',payload: json})
       }
     }
     fetchWorkouts()
-  },[])
+  },[dispatch])
   return ( 
     <div className='home'>
         <div className='workouts'>
@@ -24,6 +28,7 @@ function Home() {
             <Workoutdetails key={workout._id} workout={workout} />
           ))}
         </div>
+        <Workoutform /> 
     </div>
   )
 }
